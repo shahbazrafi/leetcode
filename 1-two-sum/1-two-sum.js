adee2580ac035e3e37f6f23f9bafdba66c870462
shahbazrafi <rafi.10539@gmail.com>
2022-09-15 01:58:52 +0100

    Time: 132 ms (47.97%), Space: 42.5 MB (67.51%) - LeetHub

--- 1-two-sum/1-two-sum.js
+++ 1-two-sum/1-two-sum.js
@@ -4,7 +4,7 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    for (let i = 0; i < nums.length; i++){
+    for (let i = 0; i < nums.length-1; i++){
         for (let j = i+1; j< nums.length; j++){
             if (nums[i]+nums[j] === target && i!==j){
                 return [i, j]
