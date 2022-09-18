4023f239a982b9da6318aa244ebe30c94ac0cb88
shahbazrafi <rafi.10539@gmail.com>
2022-09-19 00:55:07 +0100

    Time: 64 ms (96.68%), Space: 42 MB (87.92%) - LeetHub

---
+++ 1991-find-the-middle-index-in-array/1991-find-the-middle-index-in-array.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMiddleIndex = function(nums) {
+    let total = 0
+    for (let i = 0; i < nums.length; i++){
+        total += nums[i]
+    }
+    let total2 = 0
+    for (let i = 0; i < nums.length; i++){
+        if (total2 === (total-nums[i])/2){
+            return i
+        }
+        total2 += nums[i]
+    }
+    return -1
+};
