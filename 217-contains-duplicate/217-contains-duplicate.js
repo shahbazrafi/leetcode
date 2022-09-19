8d7b01d5046062f29dc56d8068145c36de258cfd
shahbazrafi <rafi.10539@gmail.com>
2022-09-19 01:03:41 +0100

    Time: 139 ms (53.44%), Space: 50.9 MB (48.78%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.js
+++ 217-contains-duplicate/217-contains-duplicate.js
@@ -3,10 +3,6 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-    for (let i = 0; i < nums.length; i++){
-        if (nums.indexOf(nums[i]) !== i){
-            return true
-        }
-    }
-    return false
+    const newSet = new Set(nums)
+    return nums.length !== newSet.size
 };
